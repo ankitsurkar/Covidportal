165db3de35f3dfd2a435322f911a04b414fefc99
Ankit Surkar <ankit_surkar@HJNZL000074C.domain.name>
2020-09-14 10:44:04 +0530

    first commit

---
+++ submit.js
@@ -0,0 +1,14 @@
+function submitFunction() {
+    var name = document.getElementById("first_3");
+    var lastname = document.getElementById("last_3");
+    var phone_no = document.getElementById("input_7_phone");
+    var blood_group = document.getElementById("blood_group");
+    var current_status = document.getElementById("current_status");
+    var needs_plasma = document.getElementById("needs_plasma");
+    var add_details = document.getElementById("additional_details");
+    var sp02 = document.getElementById("sp02");
+    var zone = document.getElementById("zone");
+    var address = document.getElementById("address");
+    var test_lab = document.getElementById("test_lab");
+    var test_date = document.getElementById("test_date");
+  }
